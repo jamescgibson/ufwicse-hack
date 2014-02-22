2cd5ca545dc1c05d434e45cc38b545b4d1791381
James Gibson <james@innovategsb.com>
2014-02-22 14:59:19 -0500

    Can return data from API
    - must use promises, because.

--- frontend/api.js
+++ frontend/api.js
@@ -1,1 +1,15 @@
-window.app_token = "WWAqNqivUpqzLb9ZQyzCkDXpj";
+window.api = {}
+api.app_token = "WWAqNqivUpqzLb9ZQyzCkDXpj";
+api.endpoint = "http://data.cityofgainesville.org/resource/dbcj-nniz.json";
+
+api.getNatGasFromServer = function(address) {
+  return $.get(api.endpoint + "?ServiceAddress=10%20NW%206TH%20ST", function(data) {
+    var result = [];
+    if(data.length > 0) {
+      result = _.map(data, function(value) {
+        value.therm_consumption;
+      });
+    }
+    return result;
+  });
+}
