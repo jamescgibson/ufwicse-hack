c4228940eca35152faa28f31f98424759a52e44e
James Gibson <james@innovategsb.com>
2014-02-22 17:06:00 -0500

    added stubbed display.js

---
+++ frontend/display.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#go").on("click", function(event) {
+    //do the thing with the address
+    event.preventDefault();
+    var address = $("#address").val();
+
+    //DO THE STUFF HERE
+  });
+});
+
+function displayElectric(address) {
+
+}
