daf6814da8ef603ff64ebd80fd39e595f161bd2e
James Gibson <james@innovategsb.com>
2014-02-22 16:41:39 -0500

    Get business name!

--- frontend/api.js
+++ frontend/api.js
@@ -5,6 +5,7 @@ api.electricEndpoint = "http://data.cityofgainesville.org/resource/gk3k-9435.jso
 api.potableEndpoint = "http://data.cityofgainesville.org/resource/bn44-jxhy.json";
 api.waterEndpoint = "http://data.cityofgainesville.org/resource/khkv-62wd.json";
 api.codeEndpoint = "http://data.cityofgainesville.org/resource/vu9p-a5f7.json";
+api.bizEndpoint = "http://data.cityofgainesville.org/resource/hk2b-em59.json";
 
 api.encodeAddress = function(address) {
   var result = address.replace(/ /g, "%20");
@@ -51,7 +52,7 @@ api.getWaterFromServer = function(address, callback) {
 }
 
 //THIS ONE DOESN'T WORK
-api.getCodeViolations = function(address, callback) {
+api.getCodeViolationsFromServer = function(address, callback) {
   console.log(api.codeEndpoint + "?Address=" + address);
   $.get(api.codeEndpoint + "?Address=" + address, function(data) {
     var result = [];
@@ -64,4 +65,12 @@ api.getCodeViolations = function(address, callback) {
   });
 }
 
-
+api.getBizFromServer = function(address, callback) {
+  console.log(api.bizEndpoint + "?location_address=" + address);
+  $.get(api.bizEndpoint+ "?location_address=" + address, function(data) {
+    if(data.length > 0) {
+      result = data[0].name;
+    }
+    callback(result);
+  });
+}
