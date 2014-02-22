bbcae03c7fb3ff2377ee30889731c82a03320b34
James Gibson <james@innovategsb.com>
2014-02-22 13:22:33 -0500

    Setting up Rails Server
    - Yes, rails is heavyweight for what we're doing here. However, I have
      more experience with it and do not want to try and learn how to use
    sinatra while building a hackathon app.

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ufwicse::Application.config.secret_key_base = '999f139e9dfdee1e5eb7901c6a44f6221ec7c5ad0a2fd9feba599a2e739c63f8dffd1fb7731691e65ed5db87c1ab6d71cadc148356044bc01b64dce0b65b1523'
