ac62c9506a2ed12652e15bfd1c8be2f9c9272aea
sararose <sara.rose.schuster@gmail.com>
2014-02-22 17:04:04 -0500

    removed model

--- frontend/model.js
+++
@@ -1,9 +0,0 @@
-function Location(address) {
-  var self = this;
-  self.address = address;
-  self.locationType;
-  self.overallRating = api.getOverallRating(overall);
-  self.electricityPercentile = api.getElectricityPercentile(self);
-  self.waterPercentile = api.getElectricityPercentile(self);
-  self.garbagePercentile = api.getElectricityPercentile(self);
-}
