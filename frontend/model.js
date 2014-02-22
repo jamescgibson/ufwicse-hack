5bdf36d907dad811260cf1779d7b0330db50f1a0
sararose <sara.rose.schuster@gmail.com>
2014-02-22 16:27:26 -0500

    started content html

--- frontend/model.js
+++ frontend/model.js
@@ -2,7 +2,7 @@ function Location(address) {
   var self = this;
   self.address = address;
   self.locationType;
-  self.overallRating = api.getOverallRating(self);
+  self.overallRating = api.getOverallRating(overall);
   self.electricityPercentile = api.getElectricityPercentile(self);
   self.waterPercentile = api.getElectricityPercentile(self);
   self.garbagePercentile = api.getElectricityPercentile(self);
